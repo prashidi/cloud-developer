855800e315e1b23277647df0306d039801e7683b
Rashidi <prashidi@38f9d332087b.ant.amazon.com>
2019-12-24 09:07:51 +0200

    adding course code

--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -8,8 +8,12 @@ import bodyParser from 'body-parser';
 import { V0MODELS } from './controllers/v0/model.index';
 
 (async () => {
-  await sequelize.addModels(V0MODELS);
-  await sequelize.sync();
+  try {
+    await sequelize.addModels(V0MODELS);
+    await sequelize.sync();
+  } catch (e){
+    console.log("Sequilize Error: ", e)
+  }
 
   const app = express();
   const port = process.env.PORT || 8080; // default port to listen
